96e35c3b9b7e096d18ef6f44d5b64d4bdcfd8dcf
aman1455 <amannegi1140@gmail.com>
2026-08-13 17:57:10 +0530

    style: double bottom gap spacing for floating player pill

--- src/App.tsx
+++ src/App.tsx
@@ -537,7 +537,7 @@ export function App() {
       </main>
 
       {/* 4. Liquid Glass Player Pill (Bottom Floating Console) */}
-      <div className="absolute bottom-5 sm:bottom-7 left-1/2 -translate-x-1/2 z-30 w-[95%] sm:w-[94%] max-w-xl pointer-events-auto">
+      <div className="absolute bottom-10 sm:bottom-14 left-1/2 -translate-x-1/2 z-30 w-[95%] sm:w-[94%] max-w-xl pointer-events-auto">
         
         {/* Station Selector Drawer Modal */}
         {showStationModal && (
